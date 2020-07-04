10849e3a8421d59f8c31b0ac45e537d6dfc7ba68
Ilia <gipermonk@bk.ru>
2020-07-04 21:08:25 +0300

    added test activity

---
+++ app/src/main/java/ru/ifmo/statapp/presentation/activity/LoginActivity.kt
@@ -0,0 +1,10 @@
+package ru.ifmo.statapp.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
